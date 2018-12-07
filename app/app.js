243ed9067147815e879ecefcb82d42a2a5dd1141
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-07 16:17:26 +0000

    Added Sass.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
 const colors = require('colors');
+const compileSass = require('express-compile-sass');
+const root = process.cwd();
 
 const PORT = process.env.PORT || 5555;
 
 express()
+  .use(compileSass({
+    root: root,
+    sourceMap: true, // Includes Base64 encoded source maps in output css
+    sourceComments: true, // Includes source comments in output css
+    watchFiles: true, // Watches sass files and updates mtime on main files for each change
+    logToConsole: false // If true, will log to console.error on errors
+  }))
+  .use(express.static(root))
   .use(express.static(path.join(__dirname, 'static')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
