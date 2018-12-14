e58d3a1d551149c2e486b150f50fffa2908826e3
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-14 15:34:42 +0000

    Find file by name (but have to put extension (i.e. .mp4) aswell at the moment.

--- app/static/js/renamer.js
+++ app/static/js/renamer.js
@@ -1,11 +1,24 @@
+var fs = require('fs');
+
 module.exports.findFile = (req, res) => {
   const content = req.body;
   res.json(content);
 
-  console.log('ALL CONTENT:');
-  console.log(content);
-  console.log('------------------');
-  console.log('Src Dir:');
-  console.log(content.srcDir);
+
   console.log('*****************');
+  console.log('RECEIVED CONTENT:');
+  console.log(content);
+  console.log('*****************');
+
+
+  checkFileExists(content.srcDir, content.oldFilename);
+}
+
+function checkFileExists(dir, filename) {
+  const path = `${dir}/${filename}`
+  if (fs.existsSync(path)) {
+    console.log(`FILE "${filename}" FOUND!`)
+  } else {
+    console.log(`Cannot find file: "${filename}"`)
+  }
 }
