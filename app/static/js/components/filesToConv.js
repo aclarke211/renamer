61939d4ba091312a35394a08cd8e8f0cf1fb5b69
Alex Clarke <aclarke211@gmail.com>
2018-12-16 15:49:38 +0000

    Accordions for files to convert section.

--- app/static/js/components/filesToConv.js
+++ app/static/js/components/filesToConv.js
@@ -1,5 +1,9 @@
 function init(appContent, componentContent, $appContainer) {
-  $appContainer.append(template(componentContent, appContent));
+  $.when(
+    $appContainer.append(template(componentContent, appContent))
+  ).done(function () {
+    activateAccordions();
+  });
 }
 
 function template(content, allContent) {
@@ -9,31 +13,45 @@ function template(content, allContent) {
 
       <h2 class="${name}-text">${content.data.title}</h2>
 
-      <div class="fields__container">
-        <div class="field">
-          <label for="${name}-all-orig-names" class="${name}-all-orig-names-label">*ALL ORIG NAMES</label>
-          <textarea class="${name}-all-orig-names-input"></textarea>
+      <div class="accordion active">
+        <h3 class="${name}-subtitle subtitle">${content.data.multipleFiles.subtitle}</h3>
+        <div class="fields__container">
+          <div class="field">
+            <label for="${name}-all-orig-names" class="${name}-all-orig-names-label">*ALL ORIG NAMES</label>
+            <textarea class="${name}-all-orig-names-input"></textarea>
+          </div>
         </div>
+        <button class="log-names-btn" onclick="logAllOrigNames()">Console Log Names</button>
       </div>
 
-      <button class="log-names-btn" onclick="logAllOrigNames()">Console Log Names</button>
 
-      <div class="fields__container">
-        <div class="field">
-          <label for="${name}-orig-name" class="${name}-text">${content.data.orig_filename.label}</label>
-          <input id="${name}-orig-name" class="${name}-orig-name" type="text" placeholder="Original Filename">
+      <div class="accordion">
+        <h3 class="${name}-subtitle subtitle">${content.data.singleFile.subtitle}</h3>
+        <div class="fields__container">
+          <div class="field">
+            <label for="${name}-orig-name" class="${name}-text">${content.data.singleFile.orig_filename.label}</label>
+            <input id="${name}-orig-name" class="${name}-orig-name" type="text" placeholder="Original Filename">
+          </div>
+
+          <div class="field">
+            <label for="${name}-new-name" class="${name}-text">${content.data.singleFile.new_filename.label}</label>
+            <input id="${name}-new-name" class="${name}-new-name" type="text" placeholder="Original Filename">
+          </div>
         </div>
-
-        <div class="field">
-          <label for="${name}-new-name" class="${name}-text">${content.data.new_filename.label}</label>
-          <input id="${name}-new-name" class="${name}-new-name" type="text" placeholder="Original Filename">
-        </div>
-
       </div>
 
 
     </div>
   `;
+}
+
+function activateAccordions() {
+  $('.accordion .subtitle').click(function () {
+    $('.accordion').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).parent().addClass('active');
+  });
 }
 
 function logAllOrigNames() {
