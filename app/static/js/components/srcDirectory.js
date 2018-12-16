607d793ea08310591a915f0a092d7e3f8235c679
Alex Clarke <aclarke211@gmail.com>
2018-12-16 14:33:54 +0000

    Select box styling.

--- app/static/js/components/srcDirectory.js
+++ app/static/js/components/srcDirectory.js
@@ -8,7 +8,7 @@ function template(content, allContent) {
   return `
     <div class="${name}__container">
 
-      <div class="fields__container column">
+      <div class="fields__container">
         <div class="field">
           <label for="${name}-path" class="${name}-text">${content.data.text}</label>
           <input id="${name}-path" class="${name}-path" type="text" placeholder="Enter default source directory.">
