771063bb8dae297e4d6a44eaacff29a96df92b8b
Alex Clarke <aclarke211@gmail.com>
2018-12-19 01:20:48 +0000

    Create content and send data into their own methods.

--- app/static/js/client/client.js
+++ app/static/js/client/client.js
@@ -14,65 +14,16 @@ function addListeners() {
   });
 
   $(`.find-single-file-btn`).click(function () {
-
-    if ($('.srcDirectory-path').val() === '') {
-      $('.srcDirectory-path').addClass('invalid');
-
-      // Scroll to the element with no input
-      document.querySelector('.srcDirectory-path').scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
-
-    $.ajax({
-      url: '/find-file',
-      type: 'POST',
-      dataType: 'json',
-      data: JSON.stringify(createFile('video')),
-      contentType: 'application/json; charset=utf-8',
-      cache: false,
-      processData: false,
-      timeout: 5000,
-      complete: function () {
-        console.log('** process COMPLETE **');
-      },
-
-      success: function (data) {
-        console.log('** process SUCCESS **');
-
-        console.log('=======================================');
-        console.log('Returned Content: ');
-        console.log(data);
-        console.log('=======================================');
-
-        $('.returned-content').children().remove();
-
-        let html = `
-          <h3 style="color: tomato">Could not find file: ${data.files[0].oldFilename}</h3>
-        `;
-
-        if (data.files[0].foundStatus) {
-          html = `
-            <h3 style="color: forestgreen">FOUND FILE: ${data.files[0].oldFilename}${data.files[0].fileType} !</h3>
-          `;
-        }
-
-        $('.returned-content').append(html);
-
-        createResultsModal(data);
-      },
-
-      error: function () {
-        console.log('** process ERROR **');
-      },
-    });
-
-
-
+    validateForms();
+    const names = [{
+      oldFilename: $('.filesToConv-orig-name').val() || 'No Old Filename provided',
+      newFilename: $('.filesToConv-new-name').val() || 'No New Filename provided',
+    }];
+    sendData('/find-file', createContent(names, 'video'));
   });
 }
 
-function createFile(type) {
+function createContent(names, type) {
   let contentToPass = {};
   let fileTypes = {};
 
@@ -85,13 +36,18 @@ function createFile(type) {
       break
   }
 
-  let files = [{
-    foundStatus: false,
-    oldFilename: $('.filesToConv-orig-name').val() || 'No Old Filename provided',
-    newFilename: $('.filesToConv-new-name').val() || 'No New Filename provided',
-    fileTypes: fileTypes,
-    srcDir: $('.srcDirectory-path').val() || 'No Source Directory supplied.'
-  }];
+  let files = [];
+
+  names.forEach((name) => {
+    const file = {
+      foundStatus: false,
+      oldFilename: name.oldFilename,
+      newFilename: name.newFilename,
+      fileTypes: fileTypes,
+      srcDir: $('.srcDirectory-path').val() || 'No Source Directory supplied.'
+    }
+    files.push(file);
+  });
 
   contentToPass.files = files;
 
@@ -99,9 +55,10 @@ function createFile(type) {
 }
 
 function createMultipleFiles(type) {
-  logAllOrigNames();
+
 }
 
+// called in 'filesToConv.js'
 function logAllOrigNames() {
   $origNamesInput = $('.filesToConv-all-orig-names-input');
 
@@ -119,6 +76,63 @@ function logAllOrigNames() {
   });
 
   console.log(seperatedNamesArray);
+}
+
+function validateForms() {
+  if ($('.srcDirectory-path').val() === '') {
+    $('.srcDirectory-path').addClass('invalid');
+
+    // Scroll to the element with no input
+    document.querySelector('.srcDirectory-path').scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+}
+
+function sendData(url, contentToPass) {
+  $.ajax({
+    url: url,
+    type: 'POST',
+    dataType: 'json',
+    data: JSON.stringify(contentToPass),
+    contentType: 'application/json; charset=utf-8',
+    cache: false,
+    processData: false,
+    timeout: 5000,
+    complete: function () {
+      console.log('Completed a request to the server.');
+    },
+
+    success: function (data) {
+      console.log('Data successfully sent');
+
+      console.log('=======================================');
+      console.log('Returned Content: ');
+      console.log(data);
+      console.log('=======================================');
+
+      $('.returned-content').children().remove();
+
+      let html = `
+          <h3 style="color: tomato">Could not find file: ${data.files[0].oldFilename}</h3>
+        `;
+
+      if (data.files[0].foundStatus) {
+        html = `
+            <h3 style="color: forestgreen">FOUND FILE: ${data.files[0].oldFilename}${data.files[0].fileType} !</h3>
+          `;
+      }
+
+      $('.returned-content').append(html);
+
+      createResultsModal(data);
+    },
+
+    error: function () {
+      console.log('** process ERROR **');
+    },
+  });
+
 }
 
 function createResultsModal(data) {
