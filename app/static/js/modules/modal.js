734147c03cac5d91e84c475234e9f77dd3f107cc
Alex Clarke <aclarke211@gmail.com>
2018-12-19 02:21:21 +0000

    Sort files in modal.

--- app/static/js/modules/modal.js
+++ app/static/js/modules/modal.js
@@ -1,5 +1,48 @@
 function showModal(data) {
-  // alert(JSON.stringify(data));
+
+  $('.modal__container').remove();
+
+  const basicModal = `
+  <div class="modal__container">
+    <h3 class="modal-title">Modal</h3>
+    <div class="returned-files__container">
+    <div class="found-files files">
+      <h4 class="status-title">FOUND FILES</h4>
+    </div>
+      <div class="missing-files files">
+        <h4 class="status-title">MISSING FILES</h4>
+      </div>
+    </div>
+    <div class="btns_container">
+      <div class="modal-btn btn">Rename Files</div>
+    </div>
+  </div>
+  `;
+
+  $('.filesToConv__container').append(basicModal);
+  sortFiles(data)
+
+}
+
+function createFileElem(file, parentElem) {
+  const fileElem = `
+  <div class="file">
+    <p class="current_filename">${file.oldFilename}</p>
+    <p class="new_filename">${file.newFilename}</p>
+  </div>
+`;
+  $(parentElem).append(fileElem);
+}
+
+function sortFiles(data) {
+  data.files.forEach((file) => {
+    if (file.foundStatus) {
+      createFileElem(file, $('.found-files'));
+    } else {
+      createFileElem(file, $('.missing-files'));
+    }
+
+  })
 }
 
 
