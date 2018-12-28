aa28bbb1a9f513666d3763c41f95c4fd606004fc
Alex Clarke <Alex.Clarke@jdplc.com>
2018-12-28 11:13:58 +0000

    Elements now load in the correct order each time.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,20 +2,33 @@ console.log('Renamer [v1.0.0]');
 
 const $appContainer = $('.components__container');
 
+// Custom setTime promise, used to set how long to wait between rendering components
+const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+
+// Use jQuery to read content from JSON file
 $.getJSON('./app/content/content.json', function (content) {
-  content.components.forEach((component, key) => {
-    $.when(
-      $.getScript(`./app/static/js/components/${component.componentName}.js`,
-        $.Deferred(function (deferred) {
-          $(deferred.resolve);
-        })
-      ).done(function () {
+
+  // Async function to make sure elements are loaded in the correct order
+  const generateElements = async () => {
+    await asyncForEach(content.components, async (component) => {
+      await waitFor(50);
+      // Read the JS file for each of the components
+      $.getScript(`./app/static/js/components/${component.componentName}.js`, function () {
         init(content.content, component, $appContainer);
-        // console.log(`Added ${component.componentName}`);
-      }));
-  });
-  $.getScript('./app/static/js/client/client.js', function () {
-    addListeners(content);
-    // console.log('ADDED LISTENERS');
-  });
+        console.log(`Added ${component.componentName}`);
+      });
+    });
+    $.getScript('./app/static/js/client/client.js', function () {
+      addListeners(content);
+      // console.log('ADDED LISTENERS');
+    });
+  }
+  generateElements();
 });
+
+// Custom forEach loop which waits for callback to be completed
+async function asyncForEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    await callback(array[i], i, array);
+  }
+}
