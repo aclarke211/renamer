1312e6fcf0c7b0f88eda700bb4377ce2c22c8b4a
Alex Clarke <Alex.Clarke@jdplc.com>
2019-06-05 17:49:35 +0100

    Access 'package.json' in 'header.js' and display the project name and project version.

--- app/static/js/components/header.js
+++ app/static/js/components/header.js
@@ -1,11 +1,17 @@
+let pjson;
+
 function init(appContent, componentContent, $appContainer) {
-  $appContainer.append(template(componentContent, appContent));
+  $.get('/getPackageJson', (data) => {
+    pjson = data;
+    $appContainer.append(template(componentContent, appContent));
+  });
 }
 
 function template(content, allContent) {
   const name = content.componentName;
   return `
     <div class="${name}__container">
+      <p class="project-version">${pjson.name} v${pjson.version}</p>
       <h1 class="${name}-text">${content.data.text}</h1>
     </div>
   `;
